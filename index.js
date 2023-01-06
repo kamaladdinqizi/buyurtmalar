559f9f0b5ec2ab7e1643ef96cc095acb10d4ecb8
kamaladdinqizi <farzonakalandarova31@gmail.com>
2023-01-06 19:44:53 +0500

    man hazr GitHup ni ulajakman

---
+++ index.js
@@ -0,0 +1,30 @@
+// console.log("hello");
+// let ball =100
+
+// if(ball < 65){
+//     console.log("yiqildingiz");
+// }else if(ball>=65 && ball<75){
+//     console.log("super");
+// }else if( ball>=75 && ball<85){
+//     console.log("KANTRAKT");
+//     }else{
+//         console.log(grant);
+//     }
+// let k= 3000
+// for (let i=1; i<2; i++){
+
+
+//   console.log(i + 'kilo kanfetni puli');
+// }
+
+let a = 27
+// let b = 3
+// let c = 2
+// let P = (2 * a) + (2 * b) + (2 * c);
+let b=27%10
+let c=(a-b)/10
+console.log(b+c);
+// let L= 6 *(a **2);
+
+// console.log(P + " perimetri");
+//console.log( V);
